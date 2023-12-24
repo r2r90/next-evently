7e6761cfe8a06781597a632e66f22379fc50c5f0
Artur AGH <artur.agh@cpcreation.fr>
2023-12-24 18:47:50 +0100

    navbar created

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import Image from "next/image";
 import {SignedIn, SignedOut, UserButton} from "@clerk/nextjs";
 import {Button} from "@/components/ui/button";
+import {NavItems} from "@/components/shared/NavItems";
+import MobileNav from "@/components/shared/MobileNav";
 
 export const Header = () => {
     return (
@@ -11,10 +13,17 @@ export const Header = () => {
                     <Image src='/assets/images/logo.svg' width={128} height={38} alt='Evently Logo' />
                 </Link>
 
+                <SignedIn>
+                    <nav className="md:flex-between hidden w-full max-w-xs">
+                        <NavItems />
+                    </nav>
+                </SignedIn>
+
                 <div className="flex w-32 justify-end gap-3">
                     <SignedIn>
                         <UserButton afterSignOutUrl='/'>
                         </UserButton>
+                        <MobileNav />
                     </SignedIn>
                   <SignedOut>
                       <Button asChild className='rounded-full size-lg'>
